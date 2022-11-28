f24652171f13b5740da4fc864d2b4c14736831f4
ahmadou dia <dia.ahmadou23@gmail.com>
2022-11-28 15:53:58 +0100

    controller and views

---
+++ app/controllers/bounties_controller.rb
@@ -0,0 +1,31 @@
+class BountiesController < ApplicationController
+  before_action :set_bounty, only: [:create, :edit, :show]
+  def index
+    Bounty.all
+  end
+
+  def new
+    @bounty = Bounty.new
+  end
+
+  def create
+    raise
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+
+  private
+
+  def bounties_params
+    params.require(:bounty).permit(:content, :price_cents, :github_repo)
+  end
+
+  def set_bounty
+    @bounty = Bounty.find(params[:id])
+  end
+end
---
+++ test/controllers/bounties_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class BountiesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
