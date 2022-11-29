316c9bac2401eb008b8d5cc0e064ebdca8142836
Arthur Foucaud <arthurfouc@gmail.com>
2022-11-29 13:18:46 +0100

    simplevalidations

--- app/models/bounty.rb
+++ app/models/bounty.rb
@@ -1,4 +1,6 @@
 class Bounty < ApplicationRecord
   belongs_to :user
   has_many :solutions
+
+  validates :content, :price_cents, :github_repo, :title, presence: true
 end
--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,4 +1,6 @@
 class Solution < ApplicationRecord
   belongs_to :bounty
   belongs_to :user
+
+  validates :content, :github_repo, presence: true
 end
