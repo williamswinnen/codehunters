a2be0fa1e33bde9c4119298730a0f613c0c592e2
thibaut roland <thibaut.roland2@gmail.com>
2022-11-29 17:06:11 +0100

    ahah

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.solution = @solution
     @message.user = current_user
+    authorize @message
     if @message.save
       ChatroomChannel.broadcast_to(
         @chatroom,
